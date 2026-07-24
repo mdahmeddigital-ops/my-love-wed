788f688ab71d1ab5d7ecd56ab7d2074f9f31e633
mdahmeddigital-ops <mdahmeddigital@gmail.com>
2026-07-24 20:04:12 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function showLove(){
+function nextPage(){
 
-document.getElementById("message").innerHTML=`
-❤️ I Love You Forever Aruuu ❤️
-<br><br>
-You are my heart 💖
-`;
+const page1=document.querySelector(".page1");
+const page2=document.querySelector(".page2");
+
+page1.style.opacity="0";
+page1.style.pointerEvents="none";
+
+setTimeout(()=>{
+
+page2.style.opacity="1";
+page2.style.pointerEvents="auto";
+
+},600);
 
 }
