bb0474695a20809f1c2f9ea6a367a08a6730ed01
mdahmeddigital-ops <mdahmeddigital@gmail.com>
2026-07-24 20:12:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,37 @@ page2.style.pointerEvents="auto";
 },600);
 
 }
+
+
+
+function hitTarget(){
+
+const arrow=document.querySelector(".arrow");
+const target=document.querySelector(".target");
+
+arrow.style.transition="1s";
+arrow.style.transform="translateY(-120px) translateX(20px) rotate(-20deg)";
+
+setTimeout(()=>{
+
+target.innerHTML="💥";
+
+setTimeout(()=>{
+
+target.innerHTML="❤️";
+
+document.querySelector(".loveText").innerHTML=`
+<h2>I Love You Aruuu ❤️</h2>
+<p>
+You are my forever.<br>
+You are my happiness.<br><br>
+
+❤️ Forever & Always ❤️
+</p>
+`;
+
+},500);
+
+},900);
+
+}
